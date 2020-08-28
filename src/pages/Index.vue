86d279fcd033719c0ac99e14b52576b27a1e1b78
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2020-08-28 11:40:15 +0200

    Remove all instances of Esther Moyo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -189,8 +189,9 @@
             <div class="flex justify-end md:w-1/2 mt-8 mb:mt-0">
               <div class="md:-mt-16 px-6 py-8 bg-gray-900 text-white border-b-4 border-teal-600">
                 <div>
-                  <h3 class="font-bold text-3xl">Esther Moyo</h3>
-                  <p class="text-gray-200 text-base mb-6">Founder and CEO</p>
+                  <p class="text-gray-200 text-base mb-6 flex">
+                    <svg viewBox="0 0 20 20" fill="currentColor" class="badge-check w-6 h-6 mr-1"><path fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                    Founder and CEO</p>
                   <p
                     class="mb-2"
                   >The Founder and CEO majored in Clinical Psychology and has been working with children since 2011.</p>
